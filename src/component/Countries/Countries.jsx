c4d1e6ad3f3dddae97bc37d7c5eaabf199b2e52b
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-04-10 02:49:18 +0600

    sorting

--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import "./Countries.css";
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -12,11 +13,14 @@ const Countries = () => {
     return (
         <div>
             <h1>Hello from countries: {countries.length}</h1>
+            <div className="countries-container">
             {
                 countries.map(country => <Country 
                 country={country} 
+                key={country.cca3}
                 />)
             }
+            </div>
         </div>
     );
 };
--- src/component/Country/Country.jsx
+++ src/component/Country/Country.jsx
@@ -5,10 +5,11 @@ const Country = (props) => {
     const {area, region, population, name, startOfWeek} = props.country;
     return (
         <div className='country'>
-            <h3>Country Name: {props.name}</h3>
-            <p>Population: {props.population}</p>
-            <p>Area: {props.area}</p>
-            <p>start of Week: {props.startOfWeek}</p>
+            <h3>Country Name: {name.common}</h3>
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p>start of Week: {startOfWeek}</p>
+            <p>Region: {region}</p>
         </div>
     );
 };
