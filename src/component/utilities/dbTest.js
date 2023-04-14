f944cc1212e3da2d3a72f25f888363230642afd1
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-04-15 03:10:14 +0600

    LS continue

--- src/component/utilities/dbTest.js
+++ src/component/utilities/dbTest.js
@@ -1,6 +1,6 @@
 const addToLS = id => {
     let countryCart = {};
-    const checkCountry = localStorage.getItem('country-cart');
+    let checkCountry = localStorage.getItem('country-cart');
 
     if (checkCountry) {
         countryCart = JSON.parse(checkCountry);
@@ -18,12 +18,25 @@ const addToLS = id => {
         countryCart[id] = 1;
     }
 
-    localStorage.setItem('country-cart', JSON.stringify(countryCart))
+    localStorage.setItem('country-cart', JSON.stringify(countryCart));
 
 }
 
 const removeCountry = id => {
+    let countryCart = {};
+    let checkCountry = localStorage.getItem('country-cart');
+    if (checkCountry) {
+        countryCart = JSON.parse(checkCountry);
+    }
+    
 
+    let amount = countryCart[id];
+    if (amount) {
+        let newAmount = amount - 1;
+        countryCart[id] = newAmount;
+    }
+    
+    localStorage.setItem('country-cart', JSON.stringify(countryCart));
     
 }
 
